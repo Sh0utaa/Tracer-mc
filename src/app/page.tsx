59c2761d9583cc975589029acc6b051fc25c120a
Sh0utaa <stevdorashvili08@gmail.com>
2026-03-02 19:24:42 +0400

    connected railway postgresql db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,7 +47,7 @@ export default function Home() {
         const result = await response.json();
         const eventId = result.id;
         router.push(
-          `https://tracer-evd-mc.web.cern.ch/?eventId=${eventId}&group=${values.group}`,
+          `https://tracer-mc.up.railway.app/?eventId=${eventId}&group=${values.group}`,
         );
       } catch (error) {
         console.error("Error submitting event:", error);
